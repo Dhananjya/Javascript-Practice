f90ae7a958aa324bb376659cdf833c6f8fb8d9ca
Dhananjya <103345209+Dhananjya@users.noreply.github.com>
2024-11-28 15:50:44 +0000

    date conversions

---
+++ .devcontainer/01_basics/date.js
@@ -0,0 +1,6 @@
+let myDate = new Date()
+console.log(myDate.toDateString())//It convert date into string form 
+console.log(myDate.toLocaleDateString())//it more trim values of dates
+console.log(myDate.getFullYear())//it only carryout year of the date
+console.log(myDate.getHours())// it is simmilar to date 
+console.log(myDate.getDay()+1)//similar to date

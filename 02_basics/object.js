d2a265d9e492287cefbbf12678df57261bb5a920
Dhananjya <103345209+Dhananjya@users.noreply.github.com>
2024-11-29 14:52:23 +0000

    object creation

---
+++ 02_basics/object.js
@@ -0,0 +1,11 @@
+//create the object in javascript 
+//we can declare object in java script just by using curly braces {}
+ JSobject = {
+    name: "Dipesh",
+    surname: "Kafle",
+    lastLogindays: ["Monday","Saturday"]
+ }
+ //to access object elements
+ console.log(JSobject.name)
+ console.log(JSobject.lastLogindays)
+ 

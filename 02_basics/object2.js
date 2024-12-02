b09ff89d71a8faa7a86939a1c8b5c8eb261ad386
Dhananjya <103345209+Dhananjya@users.noreply.github.com>
2024-12-02 17:24:07 +0000

    final changes

--- 02_basics/object2.js
+++ 02_basics/object2.js
@@ -15,3 +15,28 @@ const regulationUser = {
 }
 
 console.log(regulationUser.fullname.firstname)
+
+//how to combine objects 
+const obj1={1:"a",2: "b"};
+const obj2={3:"a",4: "b"}
+//one method using Object.assign
+obj3=Object.assign({},obj1,obj2)
+//console.log(obj3)
+//Another method using ...obj, ...obj
+obj4={...obj1,...obj2}
+console.log(obj4);
+
+//some times when we fetching data from database we get result in object form inside array so we can access them using array indexing method
+const userdata=[
+    {name: "Dp",age:23},
+    { name: "Mnsa",age:24}
+];
+console.log(userdata[1]);
+
+//we can only access key or value of the object 
+console.log(Object.keys(obj1));
+console.log(Object.values(obj1));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('addres'))
+
+
